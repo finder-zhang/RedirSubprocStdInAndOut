af05b608022d8f4e5dc81bddeff38421f7df9693
root <root@finder.ubuntu>
2014-09-05 10:03:09 +0800

    the last one I package a function with a bug,and this one I fix it

--- rtr_demo/Debug_with_Linux GCC/src/rtr_demo.d
+++
@@ -1,1 +0,0 @@
-src/rtr_demo.d src/rtr_demo.o: ../src/rtr_demo.cpp
--- rtr_demo/src/rtr_demo.cpp
+++ rtr_demo/src/rtr_demo.cpp
@@ -13,7 +13,7 @@
 using namespace std;
 
 
-int OpenSubProcess(const char* chFileName,int* pZbcStdin,int* pZbcStdout)
+int OpenSubProcessAndReplaceInOut(const char* chFileName,int* pZbcStdin,int* pZbcStdout)
 {
 	int fd_sub_in[2];		//use to cat the sub process's stdin
 	int fd_sub_out[2];
@@ -30,40 +30,58 @@ int OpenSubProcess(const char* chFileName,int* pZbcStdin,int* pZbcStdout)
 		return 0;
 	}
 
-	*pZbcStdin = fd_sub_in[0];
-	*pZbcStdout = fd_sub_out[1];
+	*pZbcStdin = fd_sub_in[1];
+	*pZbcStdout = fd_sub_out[0];
 
 	return 1;
 }
 
 int main() {
 
-	int fd_sub_in[2];		//use to cat the sub process's stdin
-	int fd_sub_out[2];
-	pipe(fd_sub_in);		//use to cat the sub process's stdout
-	pipe(fd_sub_out);
+//	int fd_sub_in[2];		//use to cat the sub process's stdin
+//	int fd_sub_out[2];
+//	pipe(fd_sub_in);		//use to cat the sub process's stdout
+//	pipe(fd_sub_out);
+//
+//	char chRead[100];		//use to test the pipe's input and output
+//	char chWrite[100];
+//
+//	pid_t pid = fork();	//make a fork
+//
+//	if ( 0 == pid ) {
+//		// fork process here
+//		dup2(fd_sub_in[0],STDIN_FILENO);		//cat the STDIN to fd_sub_in
+//		dup2(fd_sub_out[1],STDOUT_FILENO);		//cat the STDOUT to fd_sub_out
+//		execlp("/root/workspace/router_proj/zbc/Debug/zbc","",(char*)0);		//run zbc process
+//		//will not reach here
+//	}
+//
+//		//trunk code here
+//	for (char ch = 'm';ch <= 'q';++ch) {
+//		int iLen = sprintf(chWrite,"%c\r\n",ch);
+//		write(fd_sub_in[1],chWrite,iLen);		//write to fd_sub_in,then zbc process getchar()
+//		//sleep(1);
+//	}
+//
+//	read(fd_sub_out[0],chRead,50);				//zbc process getchar() and putchar(),trunk read it back
+//	for (int i=0;i<50;++i) {
+//		printf("%02x ",chRead[i]);
+//	}
+
+	int zbcIn,zbcOut;
 
 	char chRead[100];		//use to test the pipe's input and output
 	char chWrite[100];
 
-	pid_t pid = fork();	//make a fork
-
-	if ( 0 == pid ) {
-		// fork process here
-		dup2(fd_sub_in[0],STDIN_FILENO);		//cat the STDIN to fd_sub_in
-		dup2(fd_sub_out[1],STDOUT_FILENO);		//cat the STDOUT to fd_sub_out
-		execlp("/root/workspace/zbc/Debug/zbc","",(char*)0);		//run zbc process
-		//will not reach here
-	}
+	OpenSubProcessAndReplaceInOut("/root/workspace/router_proj/zbc/Debug/zbc",&zbcIn,&zbcOut);
 
 	//trunk code here
 	for (char ch = 'm';ch <= 'q';++ch) {
 		int iLen = sprintf(chWrite,"%c\r\n",ch);
-		write(fd_sub_in[1],chWrite,iLen);		//write to fd_sub_in,then zbc process getchar()
-		sleep(1);
+		write(zbcIn,chWrite,iLen);		//write to fd_sub_in,then zbc process getchar()
 	}
 
-	read(fd_sub_out[0],chRead,50);				//zbc process getchar() and putchar(),trunk read it back
+	read(zbcOut,chRead,50);				//zbc process getchar() and putchar(),trunk read it back
 	for (int i=0;i<50;++i) {
 		printf("%02x ",chRead[i]);
 	}
